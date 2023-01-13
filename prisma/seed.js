cca98c7c904f627abdcdf826cfe56e9624ace682
= <yemunRussom10@gmail.com>
2023-01-13 12:40:03 +0000

    Requirment 1 done

--- prisma/seed.js
+++ prisma/seed.js
@@ -11,7 +11,14 @@ async function seed() {
     console.log('Customer created', createdCustomer);
 
     // Add your code here
+    const createdContact = await prisma.contact.create({
+        data: {
+            phone: '037541545',
+            email: 'alice@gmail.com'
+        }
+    });
 
+    console.log('Contact created', createdContact);
 
 
 
