038ba6735f863752756b71ec04353a0d7eae28b7
akibria12 <ahkibria@hotmail.co.uk>
2023-01-13 11:40:15 +0000

    relationship error

--- prisma/seed.js
+++ prisma/seed.js
@@ -13,8 +13,14 @@ async function seed() {
 	// Add your code here
 	const createdContact = await prisma.contact.create({
 		data: {
+			customer: {
+				connect: [{ id: 1 }],
+			},
 			phone: '0121456789',
 			email: 'ahkibria@hotmail.co.uk',
+		},
+		include: {
+			customer: true, // include all posts in the returned object
 		},
 	});
 	console.log('Contact created', createdContact);
