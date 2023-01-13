441dfb9026dc39acdb112516abe9a3fde04d1605
Fatima <fxtimaahmed14@gmail.com>
2023-01-13 12:49:37 +0000

    req 4 complete

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,52 +1,52 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 async function seed() {
   const createdCustomer = await prisma.customer.create({
     data: {
-      name: 'Alice'
-    }
-  })
+      name: "Alice",
+    },
+  });
 
-  console.log('Customer created', createdCustomer)
+  console.log("Customer created", createdCustomer);
 
   // Add your code here
   const createContact = await prisma.contact.create({
     data: {
       customer: {
         connect: {
-          id: createdCustomer.id
-        }
+          id: createdCustomer.id,
+        },
       },
-      phone: '071245889',
-      email: 'alice@gmail.com'
-    }
-  })
+      phone: "071245889",
+      email: "alice@gmail.com",
+    },
+  });
 
-  const createMovie = await prisma.movie.create({
+  const createdMovie = await prisma.movie.create({
     data: {
-      title: 'Avatar 2',
-      runTimeMins: 200
-    }
-  })
+      title: "Avatar 2",
+      runTimeMins: 200,
+    },
+  });
 
   const createScreening = await prisma.screening.create({
     data: {
-      startAt: '2023-01-13T11:42:42.359Z',
+      startAt: "2023-01-13T11:42:42.359Z",
       movie: {
         connect: {
-          id: createMovie.id
-        }
-      }
-    }
-  })
+          id: createdMovie.id,
+        },
+      },
+    },
+  });
 
   // Don't edit any of the code below this line
-  process.exit(0)
+  process.exit(0);
 }
 
 seed().catch(async (error) => {
-  console.error(error)
-  await prisma.$disconnect()
-  process.exit(1)
-})
+  console.error(error);
+  await prisma.$disconnect();
+  process.exit(1);
+});
