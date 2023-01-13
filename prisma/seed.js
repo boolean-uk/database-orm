da1a64ae5348232adf81eb4509592e7cb3ab80f0
Enky <enkh6at@yahoo.com>
2023-01-13 12:36:13 +0000

    movie part

--- prisma/seed.js
+++ prisma/seed.js
@@ -21,7 +21,19 @@ async function seed() {
 		},
 	});
 	console.log('Contact created', createdContact);
-
+	const createdMovie = await prisma.movie.create({
+		data: {
+			title: 'Matrix 9',
+			runtimeMins: 120
+		},
+	});
+	console.log('Movie created', createdMovie);
+	const createdScreening = await prisma.screening.create({
+		data: {
+			startsAt: new Date(2023, 01, 14, 20, 0, 0)
+		},
+	});
+	console.log('start time', createdScreening)
 	// Don't edit any of the code below this line
 	process.exit(0);
 }
