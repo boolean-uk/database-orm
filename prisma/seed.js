5624f4c820509b4986cd570cf16c74a8c33085af
Dave <Daverevell491@gmail.com>
2023-01-13 15:28:46 +0000

    Req. 5 done

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,81 +2,102 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function seed() {
-  const createdCustomer = await prisma.customer.create({
-    data: {
-      name: "Alice",
-    },
-  });
+	const createdCustomer = await prisma.customer.create({
+		data: {
+			name: "Alice",
+		},
+	});
 
-  console.log("Customer created", createdCustomer);
+	console.log("Customer created", createdCustomer);
 
-  // Add your code here
+	// Add your code here
 
-  const createdContact = await prisma.contact.create({
-    data: {
-      phone: "07231234124",
-      email: "Alice@email.com",
-      customer: {
-        connect: {
-          id: createdCustomer.id,
-        },
-      },
-    },
-  });
+	const createdContact = await prisma.contact.create({
+		data: {
+			phone: "07231234124",
+			email: "Alice@email.com",
+			customer: {
+				connect: {
+					id: createdCustomer.id,
+				},
+			},
+		},
+	});
 
-  const createCustomerWithContact = await prisma.customer.create({
-    data: {
-      name: "Bob Tables",
-      contact: {
-        create: {
-          phone: "07876543210",
-          email: "bob@tables.com",
-        },
-      },
-    },
-  });
+	const createCustomerWithContact = await prisma.customer.create({
+		data: {
+			name: "Bob Tables",
+			contact: {
+				create: {
+					phone: "07876543210",
+					email: "bob@tables.com",
+				},
+			},
+		},
+	});
 
-  const createdMovie = await prisma.movie.create({
-    data: {
-      title: "The Godfather",
-      runtimeMins: 175,
-    },
-  });
+	const createdMovie = await prisma.movie.create({
+		data: {
+			title: "The Godfather",
+			runtimeMins: 175,
+		},
+	});
 
-  const createdScreening = await prisma.screening.create({
-    data: {
-      startsAt: 1500,
-      movie: {
-        connect: {
-          id: createdMovie.id,
-        },
-      },
-    },
-  });
+	const createdScreen = await prisma.screen.create({
+		data: {
+			screenNumber: 3,
+		},
+	});
 
-  const createMovieWithScreenings = await prisma.movie.create({
-    data: {
-      title: "Scarface",
-      runtimeMins: 170,
-      screenings: {
-        create: [
-          {
-            startsAt: 2000,
-          },
-          {
-            startsAt: 1800,
-          },
-        ],
-      },
-    },
-  });
+	const createdScreening = await prisma.screening.create({
+		data: {
+			startsAt: 1500,
+			movie: {
+				connect: {
+					id: createdMovie.id,
+				},
+			},
+			screen: {
+				connect: {
+					id: createdScreen.id,
+				},
+			},
+		},
+	});
 
-  // Don't edit any of the code below this line
-  process.exit(0);
+	const createMovieWithScreenings = await prisma.movie.create({
+		data: {
+			title: "Scarface",
+			runtimeMins: 170,
+			screenings: {
+				create: [
+					{
+						startsAt: 2000,
+						screen: {
+							connect: {
+								id: createdScreen.id,
+							},
+						},
+					},
+					{
+						startsAt: 1800,
+						screen: {
+							connect: {
+								id: createdScreen.id,
+							},
+						},
+					},
+				],
+			},
+		},
+	});
+
+	// Don't edit any of the code below this line
+	process.exit(0);
 }
 
 seed().catch(async (error) => {
-  console.error(error);
-  await prisma.$disconnect();
-  process.exit(1);
+	console.error(error);
+	await prisma.$disconnect();
+	process.exit(1);
 });
