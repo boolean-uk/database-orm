c5d891c03893ed7cbaf54b6f9fab350cd6ec82c9
akibria12 <ahkibria@hotmail.co.uk>
2023-01-13 12:52:51 +0000

    created movie and screen

--- prisma/seed.js
+++ prisma/seed.js
@@ -7,10 +7,11 @@ async function seed() {
 			name: 'Alice',
 		},
 	});
-
 	console.log('Customer created', createdCustomer);
 
 	// Add your code here
+
+	// Create Contact
 	const createdContact = await prisma.contact.create({
 		data: {
 			customer: {
@@ -21,19 +22,24 @@ async function seed() {
 		},
 	});
 	console.log('Contact created', createdContact);
+
+	// Create Movie
 	const createdMovie = await prisma.movie.create({
 		data: {
 			title: 'Matrix 9',
-			runtimeMins: 120
+			runtimeMins: 120,
 		},
 	});
 	console.log('Movie created', createdMovie);
+
+	// Ctreate Screen
 	const createdScreening = await prisma.screening.create({
 		data: {
-			startsAt: new Date(2023, 01, 14, 20, 0, 0)
+			startsAt: new Date(2023, 01, 14, 20, 0, 0),
 		},
 	});
-	console.log('start time', createdScreening)
+	console.log('start time', createdScreening);
+
 	// Don't edit any of the code below this line
 	process.exit(0);
 }
