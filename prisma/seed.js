89c7410c9ff1fa033743507546d8361cee510f0d
Luciano Simoni <lucianosimonipersonal@gmail.com>
2023-01-13 17:00:25 +0000

    6 requirement done

--- prisma/seed.js
+++ prisma/seed.js
@@ -33,7 +33,7 @@ async function seed() {
 	});
 	console.log('Movie created', createdMovie);
 
-	// Ctreate Screen
+	// Create Screen
 	const createdScreen = await prisma.screen.create({
 		data: {
 			number: 1,
@@ -41,6 +41,7 @@ async function seed() {
 	});
 	console.log('Screen created', createdScreen);
 
+	// Create Screening
 	const createdScreening = await prisma.screening.create({
 		data: {
 			startsAt: new Date(2023, 01, 14, 20, 0, 0),
@@ -53,6 +54,19 @@ async function seed() {
 		},
 	});
 	console.log('Screening created', createdScreening);
+
+	// Create Ticket
+	const createdTicket = await prisma.ticket.create({
+		data: {
+			customer: {
+				connect: { id: 1 },
+			},
+			screening: {
+				connect: { id: 1 },
+			},
+		},
+	});
+	console.log('Ticket created', createdTicket);
 
 	// Don't edit any of the code below this line
 	process.exit(0);
