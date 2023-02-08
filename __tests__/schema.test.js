cc877dfbae631efd0a95c2de68357592d7d7fee8
Julian Ng <julesnuggy@gmail.com>
2023-02-08 11:20:23 +0000

    Add test for Customer fields structure

--- __tests__/schema.test.js
+++ __tests__/schema.test.js
@@ -16,6 +16,36 @@ const getModelSortedFields = (modelName) => {
   return fields.map(f => f.name).sort();
 };
 
+const generateFieldStructure = (type, isId, isRequired, defaultValue) => ({
+  type,
+  isId,
+  isRequired,
+  defaultValue,
+});
+
+const generateCommonFieldStructures = () => ({
+  id: generateFieldStructure('Int', true, true, { name: 'autoincrement', args: [] }),
+  createdAt: generateFieldStructure('DateTime', false, true, { name: 'now', args: []}),
+  updatedAt: generateFieldStructure('DateTime', false, true, undefined),
+});
+
+const getModelFieldsStructure = (modelName) => {
+  const fields = getModelFields(modelName);
+  const res = {};
+
+  fields.map(field => {
+    const { name, type, isId, isRequired, default: defaultValue } = field;
+    res[name] = {
+      type,
+      isId,
+      isRequired,
+      defaultValue
+    };
+  });
+
+  return res;
+};
+
 describe('The Prisma Schema', () => {
   it('has all of the required models (case sensitive!)', () => {
     // GIVEN
@@ -31,13 +61,29 @@ describe('The Prisma Schema', () => {
   });
 
   describe('The Customer Model', () => {
+    const CUSTOMER = 'Customer';
+
     it('has all of the necessary fields (columns)', () => {
       // GIVEN
       const expectedFields = generateSortedFields(['name', 'contact']);
-      const actualFields = getModelSortedFields('Customer');
+      const actualFields = getModelSortedFields(CUSTOMER);
 
       // THEN
       expect(actualFields).toEqual(expectedFields);
+    });
+
+    it('has fields with the correct structures', () => {
+      // GIVEN
+      const commonFieldStructures = generateCommonFieldStructures();
+      const expectedFieldStructures = {
+        ...commonFieldStructures,
+        name: generateFieldStructure('String', false, true, undefined),
+        contact: generateFieldStructure('Contact', false, false, undefined),
+      };
+      const actualFieldStructures = getModelFieldsStructure(CUSTOMER);
+
+      // THEN
+      expect(expectedFieldStructures).toEqual(actualFieldStructures);
     });
   });
 });
