81426329d54112d6943c840b144d946a6c25b2ac
Julian Ng <julesnuggy@gmail.com>
2023-02-08 13:14:45 +0000

    Split Screening test suite out by requirement

--- __tests__/screening.test.js
+++ __tests__/screening.test.js
@@ -9,31 +9,118 @@ const {
 const SCREENING = 'Screening';
 
 describe('The Screening Model', () => {
+  describe('Requirement 3', () => {
+    it('has all of the necessary fields (columns)', () => {
+      // GIVEN
+      const expectedFields = generateSortedFields(['startsAt']);
+      const actualFields = getModelSortedFields(SCREENING);
 
-  it('has all of the necessary fields (columns)', () => {
-    // GIVEN
-    const expectedFields = generateSortedFields(['startsAt', 'movieId', 'movie', 'screenId', 'screen', 'tickets']);
-    const actualFields = getModelSortedFields(SCREENING);
+      // THEN
+      expectedFields.forEach(field => {
+        expect(actualFields).toContain(field);
+      });
+    });
 
-    // THEN
-    expect(actualFields).toEqual(expectedFields);
+    it('has fields with the correct structures', () => {
+      // GIVEN
+      const commonFieldStructures = generateCommonFieldStructures();
+      const expectedFieldStructures = {
+        ...commonFieldStructures,
+        startsAt: generateFieldStructure('DateTime', false, true),
+      };
+      const actualFieldStructures = getModelFieldsStructure(SCREENING);
+
+      // THEN
+      expect(actualFieldStructures).toMatchObject(expectedFieldStructures);
+    });
   });
 
-  it('has fields with the correct structures', () => {
-    // GIVEN
-    const commonFieldStructures = generateCommonFieldStructures();
-    const expectedFieldStructures = {
-      ...commonFieldStructures,
-      startsAt: generateFieldStructure('DateTime', false, true),
-      movieId: generateFieldStructure('Int', false, true),
-      movie: generateFieldStructure('Movie', false, true, undefined, ['movieId'], ['id']),
-      screenId: generateFieldStructure('Int', false, true),
-      screen: generateFieldStructure('Screen', false, true, undefined, ['screenId'], ['id']),
-      tickets: generateFieldStructure('Ticket', false, true, undefined, [], [], true),
-    };
-    const actualFieldStructures = getModelFieldsStructure(SCREENING);
+  describe('Requirement 4', () => {
+    it('has all of the necessary fields (columns)', () => {
+      // GIVEN
+      const expectedFields = generateSortedFields(['startsAt', 'movieId', 'movie']);
+      const actualFields = getModelSortedFields(SCREENING);
 
-    // THEN
-    expect(expectedFieldStructures).toEqual(actualFieldStructures);
+      // THEN
+      expectedFields.forEach(field => {
+        expect(actualFields).toContain(field);
+      });
+    });
+
+    it('has fields with the correct structures', () => {
+      // GIVEN
+      const commonFieldStructures = generateCommonFieldStructures();
+      const expectedFieldStructures = {
+        ...commonFieldStructures,
+        startsAt: generateFieldStructure('DateTime', false, true),
+        movieId: generateFieldStructure('Int', false, true),
+        movie: generateFieldStructure('Movie', false, true, undefined, ['movieId'], ['id']),
+      };
+      const actualFieldStructures = getModelFieldsStructure(SCREENING);
+
+      // THEN
+      expect(actualFieldStructures).toMatchObject(expectedFieldStructures);
+    });
+  });
+
+  describe('Requirement 5', () => {
+    it('has all of the necessary fields (columns)', () => {
+      // GIVEN
+      const expectedFields = generateSortedFields(['startsAt', 'movieId', 'movie', 'screenId', 'screen']);
+      const actualFields = getModelSortedFields(SCREENING);
+
+      // THEN
+      expectedFields.forEach(field => {
+        expect(actualFields).toContain(field);
+      });
+    });
+
+    it('has fields with the correct structures', () => {
+      // GIVEN
+      const commonFieldStructures = generateCommonFieldStructures();
+      const expectedFieldStructures = {
+        ...commonFieldStructures,
+        startsAt: generateFieldStructure('DateTime', false, true),
+        movieId: generateFieldStructure('Int', false, true),
+        movie: generateFieldStructure('Movie', false, true, undefined, ['movieId'], ['id']),
+        screenId: generateFieldStructure('Int', false, true),
+        screen: generateFieldStructure('Screen', false, true, undefined, ['screenId'], ['id']),
+      };
+      const actualFieldStructures = getModelFieldsStructure(SCREENING);
+
+      // THEN
+      expect(actualFieldStructures).toMatchObject(expectedFieldStructures);
+    });
+  });
+
+  describe('Requirement 6', () => {
+    it('has all of the necessary fields (columns)', () => {
+      // GIVEN
+      const expectedFields = generateSortedFields(['startsAt', 'movieId', 'movie', 'screenId', 'screen', 'tickets']);
+      const actualFields = getModelSortedFields(SCREENING);
+
+      // THEN
+      expectedFields.forEach(field => {
+        expect(actualFields).toContain(field);
+      });
+    });
+
+    it('has fields with the correct structures', () => {
+      // GIVEN
+      const commonFieldStructures = generateCommonFieldStructures();
+      const expectedFieldStructures = {
+        ...commonFieldStructures,
+        startsAt: generateFieldStructure('DateTime', false, true),
+        movieId: generateFieldStructure('Int', false, true),
+        movie: generateFieldStructure('Movie', false, true, undefined, ['movieId'], ['id']),
+        screenId: generateFieldStructure('Int', false, true),
+        screen: generateFieldStructure('Screen', false, true, undefined, ['screenId'], ['id']),
+        tickets: generateFieldStructure('Ticket', false, true, undefined, [], [], true),
+      };
+      const actualFieldStructures = getModelFieldsStructure(SCREENING);
+
+      // THEN
+      expect(actualFieldStructures).toMatchObject(expectedFieldStructures);
+    });
   });
 });
