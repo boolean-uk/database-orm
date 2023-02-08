a932d55a16838d026051b5ee6efbd4299739195b
Julian Ng <julesnuggy@gmail.com>
2023-02-08 11:15:57 +0000

    Add test that Customer fields exist

--- __tests__/schema.test.js
+++ __tests__/schema.test.js
@@ -1,6 +1,21 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+const COMMON_FIELDS = ['id', 'createdAt', 'updatedAt'];
+
+const generateSortedFields = (fields) => {
+  return [...COMMON_FIELDS, ...fields].sort();
+};
+
+const getModelFields = (modelName) => {
+  return prisma._dmmf.modelMap[modelName].fields;
+};
+
+const getModelSortedFields = (modelName) => {
+  const fields = getModelFields(modelName);
+  return fields.map(f => f.name).sort();
+};
+
 describe('The Prisma Schema', () => {
   it('has all of the required models (case sensitive!)', () => {
     // GIVEN
@@ -13,5 +28,16 @@ describe('The Prisma Schema', () => {
     expect(models).toContain('Screening');
     expect(models).toContain('Screen');
     expect(models).toContain('Customer');
+  });
+
+  describe('The Customer Model', () => {
+    it('has all of the necessary fields (columns)', () => {
+      // GIVEN
+      const expectedFields = generateSortedFields(['name', 'contact']);
+      const actualFields = getModelSortedFields('Customer');
+
+      // THEN
+      expect(actualFields).toEqual(expectedFields);
+    });
   });
 });
