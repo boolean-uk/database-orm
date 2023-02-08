9183ee967501133d5629a81ca6a90b5c25dec6e3
Julian Ng <julesnuggy@gmail.com>
2023-02-08 14:44:34 +0000

    Split Schema test out by requirement

--- __tests__/schema.test.js
+++ __tests__/schema.test.js
@@ -1,16 +1,43 @@
 const { getModelNames } = require('./helpers');
 
-describe('The Prisma Schema', () => {
-  it('has all of the required models (case sensitive!)', () => {
-    // GIVEN
-    const models = getModelNames();
+describe('The Prisma Schema',() => {
+  const models = getModelNames();
 
-    // THEN
-    expect(models).toContain('Customer');
-    expect(models).toContain('Contact');
-    expect(models).toContain('Movie');
-    expect(models).toContain('Screening');
-    expect(models).toContain('Screen');
-    expect(models).toContain('Customer');
+  describe('Requirement 1 Requirement 2', () => {
+    it('has all of the required models (case sensitive!)', () => {
+      expect(models).toContain('Customer');
+      expect(models).toContain('Contact');
+    });
+  });
+
+  describe('Requirement 3 Requirement 4', () => {
+    it('has all of the required models (case sensitive!)', () => {
+      expect(models).toContain('Customer');
+      expect(models).toContain('Contact');
+      expect(models).toContain('Movie');
+      expect(models).toContain('Screening');
+    });
+  });
+
+  describe('Requirement 5', () => {
+    it('has all of the required models (case sensitive!)', () => {
+      expect(models).toContain('Customer');
+      expect(models).toContain('Contact');
+      expect(models).toContain('Movie');
+      expect(models).toContain('Screening');
+      expect(models).toContain('Screen');
+    });
+  });
+
+  describe('Requirement 6', () => {
+    it('has all of the required models (case sensitive!)', () => {
+      expect(models).toContain('Customer');
+      expect(models).toContain('Contact');
+      expect(models).toContain('Movie');
+      expect(models).toContain('Screening');
+      expect(models).toContain('Screen');
+      expect(models).toContain('Customer');
+      expect(models).toContain('Ticket');
+    });
   });
 });
