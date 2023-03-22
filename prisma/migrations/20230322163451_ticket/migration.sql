7b749dc43b9b74e47cb2a71c7a3f725504d445d3
unknown <katdemey@outlook.com>
2023-03-22 16:50:14 +0000

    all tests passing

---
+++ prisma/migrations/20230322163451_ticket/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "Screen_number_key";
+
+-- DropIndex
+DROP INDEX "Screening_screenId_key";
+
+-- DropIndex
+DROP INDEX "Ticket_customerId_key";
+
+-- DropIndex
+DROP INDEX "Ticket_screeningId_key";
