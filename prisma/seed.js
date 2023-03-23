6463945d3ac073aebf27467fdf244204dffbddb4
Asiyeyurtkuran <asiyeyurtkuran@gmail.com>
2023-03-23 15:06:49 +0000

    Asiye -

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,13 +4,84 @@ const prisma = new PrismaClient();
 async function seed() {
     const createdCustomer = await prisma.customer.create({
         data: {
-            name: 'Alice'
+            name: 'Alice',
+            contact: {
+                create: {
+                    email: 'alice@test.com',
+                    phone: '+441231231234'
+                },
+            },
+        },
+        include: {
+            contact: true
         }
     });
 
     console.log('Customer created', createdCustomer);
 
     // Add your code here
+    const createdMovie = await prisma.movie.create({
+        data: {
+            title: 'The Old Guard',
+            runtimeMins: 128
+        }
+    })
+    console.log("created movie", createdMovie);
+
+
+    const createdScreen = await prisma.screen.create({
+        data: {
+            number: 6,
+            screenings: {
+                create: {
+                    startsAt: new Date('2023-03-22 15:51:56Z'),
+                    movie: {
+                        connect: {
+                            id: createdMovie.id
+                        },
+                    },
+                },
+            },
+        },
+        include: {
+            screenings: {
+                include: {
+                    movie: true
+                }
+            }
+        }
+    })
+    console.log(" screen created", createdScreen);
+
+
+    const createdTicket = await prisma.ticket.create({
+        data: {
+            customer: {
+                connect: {
+                    id: createdCustomer.id
+                }
+            },
+            screenings: {
+                connect: {
+                    id: createdScreen.screenings[0].id
+                },
+            },
+        },
+        include: {
+            customer: true,
+
+            screenings:{
+                include: {
+                    movie: true
+                }
+            }   
+
+        }
+
+    })
+    console.log('created ticket', createdTicket);
+
+
 
 
 
