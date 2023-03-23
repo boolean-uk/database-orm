cbfbd1baa2a481b94afbbc241debd1fc1d0ebc65
benji <benjipictor@gmail.com>
2023-03-23 13:31:19 +0000

    core finished

--- prisma/seed.js
+++ prisma/seed.js
@@ -31,11 +31,19 @@ async function seed() {
     });
 
     console.log('movie created', createdMovie);
+    const createdScreen = await prisma.screen.create({
+        data: {
+           number: 1
+        }
+    })
 
     const createdScreening = await prisma.screening.create({
         data: {
             movie: {
                 connect: {id: 1}
+            },
+            screen: {
+                connect: {id:1}
             },
             startsAt: new Date('2023-12-09 14:00:00')
         }
@@ -43,6 +51,20 @@ async function seed() {
 
     console.log('Screening created', createdScreening);
 
+    console.log('screen created', createdScreen);
+
+    const createdTicket = await prisma.ticket.create({
+        data: {
+            screening: {
+                connect: {id: 1}
+            },
+            customer: {
+                connect: {id: 1}
+            }
+        }
+    })
+
+    console.log('ticket Created', createdTicket);
     // Don't edit any of the code below this line
     process.exit(0);
 }
