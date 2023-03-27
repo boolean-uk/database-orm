4f9e461e1b4b2e3d686b41b5cbc1030def3091c4
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2023-03-27 10:33:49 +0100

    Requirement 2 done

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,20 +5,30 @@ async function seed() {
   const createdCustomer = await prisma.customer.create({
     data: {
       name: "Alice",
+      contact: {
+        create: {
+          phone: "+672338056682",
+          email: "alice-butler@aliceinwonderland.com",
+        },
+      },
     },
   });
-
   console.log("Customer created", createdCustomer);
 
-  // Add your code here
-  const createdContact = await prisma.contact.create({
+  const createdMovie = await prisma.movie.create({
     data: {
-      phone: "+447123203456",
-      email: "test@test.com",
+      title: "Alice in Wonderland",
+      runtimeMins: 108,
     },
   });
+  console.log("Movie created", createdMovie);
 
-  console.log("Contact created", createdContact);
+  const createdScreening = await prisma.screening.create({
+    data: {
+      startsAt: new Date("2023-12-31 13:00:00"),
+    },
+  });
+  console.log("Screening created", createdScreening);
 
   // Don't edit any of the code below this line
   process.exit(0);
