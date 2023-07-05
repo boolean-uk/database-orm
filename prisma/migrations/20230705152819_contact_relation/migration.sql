9d44303062bd89cd8960cf7fac32c0f0996e4040
Todd <ample-samples>
2023-07-05 16:30:39 +0100

    fixed ex4

---
+++ prisma/migrations/20230705152819_contact_relation/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `movie_id` on the `Screening` table. All the data in the column will be lost.
+  - Added the required column `movieId` to the `Screening` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Screening" DROP CONSTRAINT "Screening_movie_id_fkey";
+
+-- AlterTable
+ALTER TABLE "Screening" DROP COLUMN "movie_id",
+ADD COLUMN     "movieId" INTEGER NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Screening" ADD CONSTRAINT "Screening_movieId_fkey" FOREIGN KEY ("movieId") REFERENCES "Movie"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
