1db007ecdad2e9aa61ef8fe8f1e304caad7d15f1
Lee Kelly <leeowenkelly@gmail.com>
2023-07-05 11:40:31 +0100

    completed first part

--- prisma/seed.js
+++ prisma/seed.js
@@ -11,9 +11,14 @@ async function seed() {
     console.log('Customer created', createdCustomer);
 
     // Add your code here
+    const createdContact = await prisma.contact.create({
+        data: {
+            phone: '123456789',
+            email: 'myemail@myemail.com'
+        }
+    })
 
-
-
+    console.log('Contact created', createdContact)
 
     // Don't edit any of the code below this line
     process.exit(0);
