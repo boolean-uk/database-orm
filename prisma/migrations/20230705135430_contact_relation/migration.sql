bfc7d28dfbe86f81d518fcc6f3d425d45121e9f3
Todd <ample-samples>
2023-07-05 15:25:54 +0100

    finished ext2

---
+++ prisma/migrations/20230705135430_contact_relation/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "Customer" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Customer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" SERIAL NOT NULL,
+    "customerId" INTEGER NOT NULL,
+    "phone" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Contact_customerId_key" ON "Contact"("customerId");
+
+-- AddForeignKey
+ALTER TABLE "Contact" ADD CONSTRAINT "Contact_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/seed.js
+++ prisma/seed.js
@@ -2,24 +2,32 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function seed() {
-    const createdCustomer = await prisma.customer.create({
+    const createdContact2 = await prisma.contact.create({
         data: {
-            name: 'Alice'
+            phone: "14188909011",
+            email: "alice@email.com",
+            customer: {
+                create: {
+                    name: "Alice"
+                }
+            }
         }
-    });
+    })
 
-    console.log('Customer created', createdCustomer);
 
     // Add your code here
 
     const createdContact = await prisma.contact.create({
         data: {
             phone: "07123456789",
-            email: "test@email.com"
-            
-    }
+            email: "test@email.com",
+            customer: {
+                create: {
+                    name: "George"
+                }
+            }
+        }
     })
-
 
     // Don't edit any of the code below this line
     process.exit(0);
