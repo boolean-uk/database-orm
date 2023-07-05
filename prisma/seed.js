bee551575ab83dfe99fa0e7f4cc5c24c588e3530
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-07-05 15:57:24 +0200

    First 2 requirements complete

--- prisma/seed.js
+++ prisma/seed.js
@@ -12,8 +12,18 @@ async function seed() {
 
     // Add your code here
 
-
-
+    const createContact = await prisma.contact.create({
+        data: {
+            email: "alice@wonderland.com",
+            phone: "07766123456",
+            customer: {
+                connect: {
+                    id: 1
+                }
+            }
+        }
+    })
+    console.log('createdContect', createContact)
 
     // Don't edit any of the code below this line
     process.exit(0);
