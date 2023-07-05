0a903ce3ff7f503a7d5de6091a1ceea61671bfe7
Isabelle <isabel.tartarelli@live.it>
2023-07-05 12:41:21 +0100

    First requirement completed

---
+++ prisma/migrations/20230705114001_contact_creation/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Contact" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" VARCHAR(20) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "customerId" INTEGER NOT NULL,
+
+    CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Contact_customerId_key" ON "Contact"("customerId");
+
+-- AddForeignKey
+ALTER TABLE "Contact" ADD CONSTRAINT "Contact_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
