7d7d9172e8c589cb1d7e4a355635487cc67546b3
carolina arruda <carolinacalouroarruda@gmail.com>
2023-07-05 12:40:14 +0200

    finished first requirement

--- prisma/seed.js
+++ prisma/seed.js
@@ -11,9 +11,14 @@ async function seed() {
     console.log('Customer created', createdCustomer);
 
     // Add your code here
+    const createContact = await prisma.contact.create({
+        data: {
+            phone: '123456789',
+            email: 'myemail@myemail.com'
+        }
+    })
 
-
-
+    console.log('Contact created', createContact);
 
     // Don't edit any of the code below this line
     process.exit(0);
