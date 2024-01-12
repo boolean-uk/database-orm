9285bd32aabc3ce622ef7813c6247d4c3a8243c4
Jack Miller <127975048+jdm1991@users.noreply.github.com>
2024-01-12 15:22:45 +0000

    Tests passing

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,75 +3,97 @@ const prisma = new PrismaClient();
 
 async function seed() {
     const createdCustomer = await prisma.customer.create({
-        data: {
-            name: 'Alice'
+      data: {
+        name: 'Alice'
+      }
+    })
+  
+    console.log('Customer created', createdCustomer)
+  
+    const createdContact = await prisma.contact.create({
+      data: {
+        phone: '+31041145962',
+        email: 'test@test.com',
+        customerId: createdCustomer.id
+      }
+    })
+  
+    console.log('Contact created', createdContact)
+  
+    const createCustomerWithContact = await prisma.customer.create({
+      data: {
+        name: 'Jack',
+        contact: {
+          create: {
+            phone: '+5445525',
+            email: 'test@test.com'
+          }
         }
-    });
-
-    console.log('Customer created', createdCustomer);
-
-    // Add your code here
-
-    async function seed() {
-        const createContact = await prisma.contact.create({
-            data: {
-                email: 'jimbo123@gmail.com',
-                phone: '123',
-                customerId: createdCustomer.id
+      },
+      include: {
+        contact: true
+      }
+    })
+  
+    console.log('New Customer created', createCustomerWithContact)
+  
+    const createMovie = await prisma.movie.create({
+      data: {
+        title: 'Teeth',
+        runtimeMins: 110
+      }
+    })
+  
+    console.log('New Movie created', createMovie)
+  
+    const startTime = new Date()
+  
+    const createScreen = await prisma.screen.create({
+      data: {
+        number: 1
+      }
+    })
+  
+    console.log('New screen created', createScreen)
+  
+    const createScreening = await prisma.screening.create({
+      data: {
+        startsAt: startTime,
+        movieId: createMovie.id,
+        screenId: createScreen.id
+      }
+    })
+  
+    console.log('New screening created', createScreening)
+  
+    const createMovieWithScreening = await prisma.movie.create({
+      data: {
+        title: 'Nemo',
+        runtimeMins: 111,
+        screenings: {
+          create: [
+            {
+              startsAt: startTime,
+              screenId: createScreen.id
             }
-        });
-    
-        console.log('Contact created', createContact);
-
-
-        const createMovie = await prisma.movie.create({
-            data: {
-              title: 'Teeth',
-              runtimeMins: 110
-            }
-          })
-        
-          console.log('New Movie created', createMovie)
-        
-          const startTime = new Date()
-        
-          const createScreen = await prisma.screen.create({
-            data: {
-              number: 1
-            }
-          })
-        
-          console.log('New screen created', createScreen)
-
-          const createdScreening = await prisma.screening.create({
-            data: {
-              startsAt: new Date("2024-09-22 15:10:00"),
-              movieId: createdMovie.id,
-            },
-          });
-        
-          console.log("Screening created", createdScreening);
-        
-          const createMovieWithScreenings = await prisma.movie.create({
-            data: {
-              title: "Another Movie",
-              runtimeMins: 120,
-              screenings: {
-                create: [
-                  {
-                    startsAt: new Date("2025-11-12 21:15:00"),
-                  },
-                ],
-              },
-            },
-          });
-
-
-
-
-
-
-
+          ]
+        }
+      },
+      include: {
+        screenings: true
+      }
+    })
+  
+    console.log('New movie created with screening', createMovieWithScreening)
+  
+    const createdTicket = await prisma.ticket.create({
+      data: {
+        customerId: createdCustomer.id,
+        screeningId: createScreening.id
+      }
+    })
+  
+    console.log('New ticket', createdTicket)
     // Don't edit any of the code below this line
     process.exit(0);
 }}
