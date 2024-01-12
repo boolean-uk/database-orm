6b2f93b998e07be2fd4e236a3f4ff7060bf1145c
Loza_TM <lozatmamo@gmail.com>
2024-01-13 00:31:35 +0100

    unique index created

---
+++ prisma/migrations/migrations.sql
@@ -0,0 +1,16 @@
+// this DB created on elephantsql instance successfully 
+CREATE TABLE "Contact" (
+    "id" SERIAL NOT NULL,
+    "phone" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+--alter table added 
+ALTER TABLE "Contact" ADD COLUMN "customerId" INTEGER NOT NULL;
+
+    CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
+);
+
+-- creating unique index on contact using customenr id 
+CREATE UNIQUE INDEX "Contact_customerId_key" ON "Contact"("customerId");
