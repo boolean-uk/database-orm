5c7cf5971a14962642489330b1c19452b5f53a2c
Kye <NovusAa12@proton.me>
2024-01-12 16:14:46 +0000

    add req.1

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,27 +1,32 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function seed() {
-    const createdCustomer = await prisma.customer.create({
-        data: {
-            name: 'Alice'
-        }
-    });
+  const createdCustomer = await prisma.customer.create({
+    data: {
+      name: "Alice",
+    },
+  });
 
-    console.log('Customer created', createdCustomer);
+  console.log("Customer created", createdCustomer);
 
-    // Add your code here
+  // Add your code here
 
+  const contact = await prisma.contact.create({
+    data: {
+      phone: "+4407123456789",
+      email: "realperson@email.com",
+    },
+  });
 
+  console.log("Contact created", contact);
 
-
-    // Don't edit any of the code below this line
-    process.exit(0);
+  // Don't edit any of the code below this line
+  process.exit(0);
 }
 
-seed()
-    .catch(async (error) => {
-        console.error(error);
-        await prisma.$disconnect();
-        process.exit(1);
-    })
+seed().catch(async (error) => {
+  console.error(error);
+  await prisma.$disconnect();
+  process.exit(1);
+});
