98e165a97d1341f8120d383b32138852a29f41af
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-12 12:38:14 +0000

    build: add correlated record for contact-customer

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,33 +1,47 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function seed() {
-    const createdCustomer = await prisma.customer.create({
-        data: {
-            name: 'Alice'
+  const createdCustomer = await prisma.customer.create({
+    data: {
+      name: "Alice",
+    },
+  });
+
+  console.log("Customer created", createdCustomer);
+
+  // Add your code here
+
+  const createdContact = await prisma.contact.create({
+    data: {
+      phone: "0736509060",
+      email: "alice.sth@gmail.com",
+    },
+  });
+
+  console.log("Created contact:", createdContact);
+
+  const createdContactWithCustomerRelation = await prisma.customer.create({
+    data: {
+        phone: "0798769032",
+        email: "bob.sth@gmail.com",
+        customer: {
+            create: {
+                name: "John"
+            }
         }
-    });
+    },
+  });
 
-    console.log('Customer created', createdCustomer);
+  console.log("Created contact with customer:", createdContactWithCustomerRelation);
 
-    // Add your code here
 
-    const createdContact = await prisma.contact.create({
-        data: {
-            phone: '0736509060',
-            email: 'alice.sth@gmail.com'
-        }
-    })
-
-    console.log('Created contact:', createdContact)
-
-    // Don't edit any of the code below this line
-    process.exit(0);
+  // Don't edit any of the code below this line
+  process.exit(0);
 }
 
-seed()
-    .catch(async (error) => {
-        console.error(error);
-        await prisma.$disconnect();
-        process.exit(1);
-    })
+seed().catch(async (error) => {
+  console.error(error);
+  await prisma.$disconnect();
+  process.exit(1);
+});
