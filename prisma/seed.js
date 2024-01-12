8070cddcc1993291393778f899d56761e094a4fc
ilham saleh <ilham2saleh@gmail.com>
2024-01-12 13:57:24 +0000

    Requirement 1 - Create model contact and record for contact model

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,27 +1,27 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function seed() {
-    const createdCustomer = await prisma.customer.create({
-        data: {
-            name: 'Alice'
-        }
-    });
+  const createdCustomer = await prisma.customer.create({
+    data: {
+      name: "Alice",
+    },
+  });
 
-    console.log('Customer created', createdCustomer);
+  // Add your code here
+  const createdContact = await prisma.contact.create({
+    data: {
+      phone: "+447745757561",
+      email: "example@gmail.com",
+    },
+  });
 
-    // Add your code here
-
-
-
-
-    // Don't edit any of the code below this line
-    process.exit(0);
+  // Don't edit any of the code below this line
+  process.exit(0);
 }
 
-seed()
-    .catch(async (error) => {
-        console.error(error);
-        await prisma.$disconnect();
-        process.exit(1);
-    })
+seed().catch(async (error) => {
+  console.error(error);
+  await prisma.$disconnect();
+  process.exit(1);
+});
