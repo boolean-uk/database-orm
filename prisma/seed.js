8a14f2565d2780c3ebb5db2b4cf029368ec4966d
Nazar Tymiv <nazartumiv@gmail.com>
2024-01-13 22:34:32 +0000

    Add Movie and Screening models

--- prisma/seed.js
+++ prisma/seed.js
@@ -26,6 +26,24 @@ async function seed() {
 
   console.log('Contact created', createContact)
 
+  // Movie and Screening
+  const createMovie = await prisma.movie.create({
+    data: {
+      title: 'Movie 1',
+      runtimeMins: 90
+    }
+  })
+
+  console.log('Movie created', createMovie)
+
+  const createScreening = await prisma.screening.create({
+    data: {
+      startsAt: new Date()
+    }
+  })
+
+  console.log('Screening created', createScreening)
+
   // Don't edit any of the code below this line
   process.exit(0)
 }
