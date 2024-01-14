edd73b45880ec2ca4e43e789ed8e4e827d95b36f
Atif Furqaan <atifurqaan@gmail.com>
2024-01-14 12:44:15 +0000

    Completed

---
+++ prisma/migrations/20240112153324_movie_screening_and_relations/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "runtimeMins" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Screening" (
+    "id" SERIAL NOT NULL,
+    "startsAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "movie_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Screening_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Screening" ADD CONSTRAINT "Screening_movie_id_fkey" FOREIGN KEY ("movie_id") REFERENCES "Movie"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
