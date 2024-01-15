d75956d57ed7fe83bec3ce541ddd86422287dc03
aayushlama4008 <aayushlama4008@gmail.com>
2024-01-16 00:49:30 +0100

    added models with seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -26,9 +26,18 @@ async function seed() {
     console.log('new contact',createContact)
 
     const createMovie = await prisma.movie.create({
-        
+        data:{
+            title:'One piece',
+            runtimeMins:150
+        }
     })
 
+    const createScreening = await prisma.screening.create({
+        data:{
+            startsAt:new Date('2024-01-16 00:45:00.000') 
+        }
+    })
+    
 
 
     // Don't edit any of the code below this line
