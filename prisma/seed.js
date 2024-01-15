88b91e6ab12eed1d92c04501806146ee5d2b911a
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2024-01-15 15:22:55 +0100

    one to many relationship movie to screening

--- prisma/seed.js
+++ prisma/seed.js
@@ -51,6 +51,17 @@ async function seed() {
     },
   });
   console.log("screening", screenings);
+
+  const screen = await prisma.screen.create({
+    data: {
+      number: 1,
+      screenings: {
+        connect: {
+          id: 1,
+        },
+      },
+    },
+  });
   // Don't edit any of the code below this line
   process.exit(0);
 }
