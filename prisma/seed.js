b1a0dbc3132b12b5c875b18d8084a16a02f3e1f1
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-06-27 13:16:54 +0200

    add contact relation

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,20 +4,17 @@ const prisma = new PrismaClient();
 async function seed() {
     const createdCustomer = await prisma.customer.create({
         data: {
-            name: 'Alice'
+            name: 'Alice',
+            contact: {
+                create: {
+                    phone: '003156879',
+                    email: 'someemail'
+                }
+            }
         }
     });
 
     console.log('Customer created', createdCustomer);
-
-    const createdContact = await prisma.contact.create({
-        data: {
-            phone: '003156879',
-            email: 'someemail'
-        }
-    });
-
-    console.log('Contact created', createdContact);
 
     process.exit(0);
 }
