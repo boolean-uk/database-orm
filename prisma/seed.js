2f8cd842c1730a91cb5eed60feccf4b3af9bdf6c
Leonardo Lodi <your_email@address>
2024-06-27 18:09:26 +0100

    add movie and screening models

--- prisma/seed.js
+++ prisma/seed.js
@@ -21,6 +21,23 @@ async function seed() {
 
     console.log('Contact created', createdContact);
 
+    const createdMovie = await prisma.movie.create({
+        data: {
+            title: 'new movie',
+            runtimeMins: 90
+        }
+    })
+
+    console.log('Movie created', createdMovie)
+
+    const createdScreening = await prisma.screening.create({
+        data: {
+            startsAt: '2024-06-27T16:58:05.495Z'
+        }
+    })
+
+    console.log('Screening created', createdScreening)
+
     // Don't edit any of the code below this line
     process.exit(0);
 }
