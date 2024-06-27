898c93fbc64f13467ebd8667f2624fe65f0c71e8
Will Baxter <bax.will@googlemail.com>
2024-06-27 15:34:17 +0100

    2nd requirement completed

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,19 +2,28 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function seed() {
-    const createdCustomer = await prisma.customer.createMany({
-        data: [{
-            name: 'Alice'
-        }, {
-            name: 'John'
-        }]
+    const createdCustomer = await prisma.customer.create({
+        data: {
+            name: 'Alice',
+            contact: {
+                create: {
+                    email: 'alice@hotmail.com',
+                    phone: '07526961405'
+                }
+            }
+        }
     });
 
     console.log('Customer created', createdCustomer);
 
     // Add your code here
 
-
+    // const createdContact = await prisma.contact.create({
+    //     data: {
+    //         email: 'shane@charlesworth.com',
+    //         phone: '07526396456',
+    //     }
+    // })
 
 
     // Don't edit any of the code below this line
