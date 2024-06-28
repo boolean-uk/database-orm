f280c9faf977b0af8965de5db8dcb6c8c45ba54a
Hamada Abdelaal <hamada_abdelaal@hotmail.com>
2024-06-28 20:04:09 +0200

    all requirements

---
+++ prisma/migrations/20240628170852_create_movie_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "runtimeMins" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
+);
---
+++ prisma/migrations/20240628171204_create_screening_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Screening" (
+    "id" SERIAL NOT NULL,
+    "startsAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Screening_pkey" PRIMARY KEY ("id")
+);
