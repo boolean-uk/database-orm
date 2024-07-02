f0d8c11403404f65610afa151bc61c87b5bc5dfc
Farshad <f.bagdeli13@gmail.com>
2024-07-02 13:38:16 +0200

    requirements2 Done

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,17 +1,34 @@
 const { PrismaClient } = require('@prisma/client');
+const { empty } = require('@prisma/client/runtime/library');
 const prisma = new PrismaClient();
 
 async function seed() {
     const createdCustomer = await prisma.customer.create({
         data: {
-            name: 'Alice'
-        }
-    });
+            name: 'Farshad',
+            contact: {
+                create: {
+                email: 'f.bagdeli',
+                phone: '123456'
+                }
+            }
+        },
+        include : {
+            contact : true
+        } 
+    })
+
+    
 
     console.log('Customer created', createdCustomer);
 
     // Add your code here
-
+    // const createdContact = await prisma.contact.create({
+    //     data: {
+    //         phone : '123456',
+    //         email : 'fBagdeli@gmail.com'
+    //     }
+    // })
 
 
 
