525e3e6098550c049fd52f610fc9f760b3fb039a
Periklis <perik76@gmail.com>
2024-07-07 23:46:00 +0300

    finish 2

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,25 +5,27 @@ async function seed() {
 	const createdCustomer = await prisma.customer.create({
 		data: {
 			name: "Alice",
-        },
-        contact: {
-            create: {
-            phone: '1234567',
-            email: 'alice@alice.al'
-        }
+            contact: {
+                create: {
+                    phone: "1234567",
+                    email: "alice@alice.al",
+                },
+            },
+        }, include: {
+            contact: true
         }
 	})
 
 	console.log("Customer created", createdCustomer)
 
-    // Add your code here
-    
-    // const createContact = await prisma.contact.create({
-    //     data: {
-    //         phone: '1234567',
-    //         email: 'alice@alice.al'
-    //     }
-    // })
+	// Add your code here
+
+	// const createContact = await prisma.contact.create({
+	//     data: {
+	//         phone: '1234567',
+	//         email: 'alice@alice.al'
+	//     }
+	// })
 
 	// Don't edit any of the code below this line
 	process.exit(0)
