18a14f8b48b1a9504a6a3f0c8b3428d3d2cf15d5
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-08-07 17:40:09 +0100

    Added movie and screening models

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,29 +2,50 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function seed() {
-    const createdCustomer = await prisma.customer.create({
-        data: {
-            name: 'Alice'
-        }
-    });
-
-    console.log('Customer created', createdCustomer);
-
-    // Add your code here
-
-    const createdContact = await prisma.contact.create({
-      data: {
-        phone: '000-111-222-333',
-        email: 'alice123@gmail.com',
+  const createdCustomer = await prisma.customer.create({
+    data: {
+      name: "Alice",
+      contact: {
+        create: {
+          phone: "123-456-789-101",
+          email: "new-email@gmail.com",
+        },
       },
-    });
+    },
+  });
 
-    console.log("Contact created", createdContact);
+  console.log("Customer created", createdCustomer);
 
+  // Add your code here
 
+  // const createdContact = await prisma.contact.create({
+  //   data: {
+  //     phone: '000-111-222-333',
+  //     email: 'alice123@gmail.com',
+  //   },
+  // });
 
-    // Don't edit any of the code below this line
-    process.exit(0);
+  // console.log("Contact created", createdContact);
+
+  const createdMovie = await prisma.movie.create({
+    data: {
+      title: 'Shrek',
+      runtimeMins: 90,
+    },
+  });
+
+  console.log("Movie created", createdMovie);
+
+  const createdScreening = await prisma.screening.create({
+    data: {
+      startsAt: '2024-07-07T11:30:00Z'
+    },
+  });
+
+  console.log("Screening created", createdScreening);
+
+  // Don't edit any of the code below this line
+  process.exit(0);
 }
 
 seed()
