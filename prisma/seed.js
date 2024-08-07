82970f6745d2ea32188519be0ed2c08fa5eeea57
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-08-07 18:25:32 +0100

    Added ticket model with data

--- prisma/seed.js
+++ prisma/seed.js
@@ -31,28 +31,38 @@ async function seed() {
     data: {
       title: "Shrek",
       runtimeMins: 90,
-      screenings: {
-        create: {
-          startsAt: "2024-07-07T11:30:00Z",
-          screen: {
-            create: {
-              number: 4,
-            },
-          },
-        },
+    }
+});
+
+console.log("Movie created", createdMovie);
+
+const createdScreen = await prisma.screen.create({
+    data: {
+        number: 3
+    }
+});
+
+console.log("Screen created", createdScreen);
+
+
+    const createdScreening = await prisma.screening.create({
+      data: {
+        startsAt: '2024-07-07T11:30:00Z', 
+        movieId: createdMovie.id,
+        screenId: createdScreen.id
       },
-    },
-  });
+    });
 
-  console.log("Movie created", createdMovie);
+    console.log("Screening created", createdScreening);
 
-  //   const createdScreening = await prisma.screening.create({
-  //     data: {
-  //       startsAt: '2024-07-07T11:30:00Z'
-  //     },
-  //   });
+    const createdTicket = await prisma.ticket.create({
+        data: {
+        customerId: createdCustomer.id,
+        screeningId: createdScreening.id
+        }
+    });
 
-  //   console.log("Screening created", createdScreening);
+     console.log("Ticket created", createdTicket);
 
   // Don't edit any of the code below this line
   process.exit(0);
